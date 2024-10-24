19dc351333e140a8d09207eba42990b9d277ac3a
naga <mailtovnrajan@gmail.com>
2024-10-24 20:43:46 +0100

    Add StatusControllerUnitTest configs

--- src/test/java/com/backendify/proxy/controller/StatusControllerUnitTest.java
+++ src/test/java/com/backendify/proxy/controller/StatusControllerUnitTest.java
@@ -1,15 +1,20 @@
 package com.backendify.proxy.controller;
 
+import com.backendify.proxy.application.Application;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-@WebMvcTest(StatusController.class)  // Test only the controller layer
+@SpringBootTest(classes = Application.class)
+@AutoConfigureMockMvc
+@ComponentScan(basePackages = "com.backendify.proxy")
 public class StatusControllerUnitTest {
 
     @Autowired
