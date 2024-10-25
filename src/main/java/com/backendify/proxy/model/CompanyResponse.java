7fc3fc82bfd89bb0b264b08474087b7b6351c36d
naga <mailtovnrajan@gmail.com>
2024-10-25 13:40:34 +0100

    Modify CompanyResponse to avoid non null fields being passed

--- src/main/java/com/backendify/proxy/model/CompanyResponse.java
+++ src/main/java/com/backendify/proxy/model/CompanyResponse.java
@@ -1,5 +1,8 @@
 package com.backendify.proxy.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CompanyResponse {
 
     private String id;
